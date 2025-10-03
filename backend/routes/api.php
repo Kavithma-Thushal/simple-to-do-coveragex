c03e88a5e710109b6d3be478ac29fcafcc72f884
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-10-03 21:59:24 +0530

    updated

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('tasks')->group(function () {
+Route::prefix('task')->group(function () {
     Route::post('create', [TaskController::class, 'create']);
     Route::get('read', [TaskController::class, 'read']);
     Route::patch('{task}', [TaskController::class, 'update']);
